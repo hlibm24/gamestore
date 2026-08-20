1d0ca08d994a0a17cf58f6dfac6b4e5f20899e05
hlib <gleb.moiseienko@gmail.com>
2026-08-20 14:53:35 +0200

    feat: add system requirements to games data and Game type

--- src/type/Game.ts
+++ src/type/Game.ts
@@ -1,3 +1,12 @@
+  interface SystemRequirements {
+    OS: string,
+    PROCESSOR: string,
+    MEMORY: string,
+    GRAPHICS: string,
+    DIRECTX: string,
+    STORAGE: string
+  }
+  
   export interface Game {
     appID: number,
     name: string,
@@ -8,6 +17,8 @@
     price: number,
     detailed_description: string,
     short_description: string,
+    minimum_requirements: SystemRequirements,
+    recommended_requirements: SystemRequirements,
     supported_languages: string,
     full_audio_languages: string,
     reviews: string,
