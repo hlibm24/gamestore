cbc06adad765da32fd5f2ecc3aab8f2a49f50742
hlib <gleb.moiseienko@gmail.com>
2026-08-26 14:23:25 +0200

    feat: wire up add-to-cart button in PurchaseBox

--- src/components/PurchaseBox.tsx
+++ src/components/PurchaseBox.tsx
@@ -1,15 +1,27 @@
 import { type Game } from "../type/Game";
+import { useCart } from "../context/CartContext";
 
 interface PurchaseBoxProps {
     game: Game;
 }
 
 export const PurchaseBox = ({game}:PurchaseBoxProps) => {
+    const {addToCart} = useCart();
+
+    const handleAddToCart = () => {
+        addToCart({
+            appID: game.appID,
+            name: game.name,
+            header_image: game.header_image,
+            price: game.price,
+        })
+    }
+
     return (
         <div className="purchase-box">
             <h3>{game.price}</h3>
             <button>Buy now</button>
-            <button>Add to card</button>
+            <button onClick={handleAddToCart}>Add to card</button>
         </div>
     )
 }
