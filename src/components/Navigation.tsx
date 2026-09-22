2e9bffd1b0e06fea8754023f03aacd660fe75488
hlib <gleb.moiseienko@gmail.com>
2026-09-22 12:50:28 +0200

    feat: hide search bar on cart, library and wallet pages

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,19 @@
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import { SearchBar } from './SearchBar';
 
+const hiddenSearchPaths = ['/cart', '/library', '/wallet'];
+
 export const Navigation = () => {
+    const location = useLocation();
+    const showSearch = !hiddenSearchPaths.includes(location.pathname);
+    
     return (
         <nav>
             <Link to='/'>Store</Link>
             <Link to='/library'>Library</Link>
             <Link to='/cart'>Cart</Link>
             <Link to='/wallet'>Wallet</Link>
-            <SearchBar />
+            {showSearch && <SearchBar />}
         </nav>
     );
 };
